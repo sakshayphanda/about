bad6ca272bf43a8137bf8e975b676b7240db4a37
sakshay phanda <sakshayphanda03@gmail.com>
2020-03-26 09:53:21 +0530

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sakshayPhanda';
+
+  itemList = [];
+  selectedItems = [];
+  settings = {};
+
+  constructor() {
+
+  }
+  ngOnInit() {
+
+
+    this.itemList = [
+      { "id": 1, "itemName": "India", "category": "asia" },
+      { "id": 2, "itemName": "Singapore", "category": "asia pacific" },
+      { "id": 3, "itemName": "Germany", "category": "Europe" },
+      { "id": 4, "itemName": "France", "category": "Europe" },
+      { "id": 5, "itemName": "South Korea", "category": "asia" },
+      { "id": 6, "itemName": "Sweden", "category": "Europe" }
+    ];
+
+    this.selectedItems = [
+      { "id": 1, "itemName": "India" },
+      { "id": 2, "itemName": "Singapore" },
+      { "id": 4, "itemName": "Canada" }];
+    this.settings = {
+      singleSelection: false,
+      text: "Select Fields",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      searchPlaceholderText: 'Search Fields',
+      enableSearchFilter: true,
+      badgeShowLimit: 5,
+      groupBy: "category",
+
+    };
+  }
+  loadDataSet1(){
+    this.selectedItems = [];
+    this.itemList = [ { "id": 1, "itemName": "Apple", "category": "fruits" },
+      { "id": 2, "itemName": "Banana", "category": "fruits" },
+      { "id": 5, "itemName": "Tomatoe", "category": "vegetables" },
+      { "id": 6, "itemName": "Potatoe", "category": "vegetables" }];
+  }
+  loadDataSet2(){
+    this.selectedItems = [];
+    this.itemList = [
+      { "id": 1, "itemName": "India", "category": "asia" },
+      { "id": 2, "itemName": "Singapore", "category": "asia pacific" },
+      { "id": 3, "itemName": "Germany", "category": "Europe" },
+      { "id": 4, "itemName": "France", "category": "Europe" },
+      { "id": 5, "itemName": "South Korea", "category": "asia" },
+      { "id": 6, "itemName": "Sweden", "category": "Europe" }
+    ];
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularMultiSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
