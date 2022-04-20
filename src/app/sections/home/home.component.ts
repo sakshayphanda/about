8d57d66d3807b811cdaa3f0f4d3a59a2c876c922
Sakshay Phanda <sakshayphanda03@gmail.com>
2022-04-20 19:33:10 +0530

    fix

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,43 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { GlobalDataService } from 'src/app/services/global-data.service';
+import { Component, OnInit } from "@angular/core";
+import { GlobalDataService } from "src/app/services/global-data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.sass"],
 })
 export class HomeComponent implements OnInit {
-
   showMenuButton = false;
   showSideMenu = true;
-  constructor(
-    private globalData: GlobalDataService
-  ) { }
+  constructor(private globalData: GlobalDataService) {}
 
   ngOnInit(): void {
-    if(window.innerWidth < 900) {
-      this.showMenuButton = true;
-      this.showSideMenu = false;
-    } else {
-      this.showMenuButton = false;
-      this.showSideMenu = true;
-    }
-
-
-    this.globalData.toggleSideMenu.subscribe(
-      response => {
-        if(window.innerWidth < 900) {
-        this.showSideMenu = !this.showSideMenu;
-        }
+    setTimeout(() => {
+      const width = Math.max(window.screen.width, window.innerWidth);
+      if (width < 900) {
+        this.showMenuButton = true;
+        this.showSideMenu = false;
+      } else {
+        this.showMenuButton = false;
+        this.showSideMenu = true;
       }
-    );
+    }, 0);
 
+    this.globalData.toggleSideMenu.subscribe((response) => {
+      if (window.innerWidth < 900) {
+        this.showSideMenu = !this.showSideMenu;
+      }
+    });
 
-
-    window.addEventListener('resize', (ev) => {
+    window.addEventListener("resize", (ev) => {
       console.log(ev);
 
-      if(ev[`target`][`innerWidth`] < 900) {
+      if (ev[`target`][`innerWidth`] < 900) {
         this.showMenuButton = true;
         this.showSideMenu = false;
       } else {
@@ -46,5 +41,4 @@ export class HomeComponent implements OnInit {
       }
     });
   }
-
 }
